28e088bae56b75d25b36bc01e93e2bc9226365fa
LeeLoo3434 <lolarussell347@gmail.com>
2023-02-01 11:17:13 -0800

    oki done for now

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,7 +2,7 @@ const Blog = require("../models/blog");
 
 module.exports = {
     create,
-
+    delete:deleteTodo
 };
 
 function create(req, res) {
@@ -13,4 +13,17 @@ function create(req, res) {
             res.redirect(`/blogs/${blog._id}`)
         })
     })
+}
+
+
+async function deleteTodo(req, res, next) {
+    try {
+        const blog = await Blog.findOne({ 'todos._id': req.params.id, 'todos.user': req.user._id })
+        if(!blog) return res.redirect('/blogs')
+        blog.todos.remove(req.params.id)
+        await blog.save()
+        res.redirect(`/blogs/${blog._id}`)
+    } catch(err) {
+        return next(err)
+    }
 }
--- routes/todos.js
+++ routes/todos.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const todosCtrl = require('../controllers/todos')
 
 router.post('/blogs/:id/todos', todosCtrl.create)
+router.delete('/todos/:id', todosCtrl.delete)
 
 module.exports = router;
